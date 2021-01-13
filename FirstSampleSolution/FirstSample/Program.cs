b898bb7be5efc241bc65daaeb372aabfb598eed1
RIbardaloza1 <77073160+RIbardaloza1@users.noreply.github.com>
2021-01-13 12:58:46 -0700

    Learning FirstSample

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// The Writeline() command will output the
+         //      the to the console window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This section is/t OA03");
+            Console.WriteLine("Your instructor name is /T Don Welch");
+        }  
+    }
+}
---
+++ MyFirstPlan/MyFirstPlan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstPlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Put card in");
+        }
+    }
+}
