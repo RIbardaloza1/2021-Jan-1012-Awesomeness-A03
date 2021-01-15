76753fdc70392762abe4d917522fd5ea61dd1c33
RIbardaloza1 <77073160+RIbardaloza1@users.noreply.github.com>
2021-01-15 11:50:14 -0700

    Spacing changes

--- Excercise01_RIbardalozaSolution/Excercise01_RIbardaloza/Program.cs
+++ Excercise01_RIbardalozaSolution/Excercise01_RIbardaloza/Program.cs
@@ -9,9 +9,9 @@ namespace Excercise01_RIbardaloza
             Console.WriteLine("Hello stranger, my name is Russell Ibardaloza and I am in" +
                 " the Computer Software Development here at NAIT.");
             Console.WriteLine("Fun fact about my self I am very good at playing League of Legends.");
-
-            Console.WriteLine("I am expecting a passing grade in this class so that I can get on my" +
-                "  next required course and pass those as well.");
+            Console.WriteLine("I am expecting a passing grade in this class so that I can get on to my" +
+                " next required courses and pass those as well.");
+          
 
             
         }
