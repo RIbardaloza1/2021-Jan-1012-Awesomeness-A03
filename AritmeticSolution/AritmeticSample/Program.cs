b3aa2f5f987812713abb73c62b1aa7eb39d787f5
RIbardaloza1 <77073160+RIbardaloza1@users.noreply.github.com>
2021-01-15 11:18:56 -0700

    InputOutput Math Conversion

---
+++ AritmeticSolution/AritmeticSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AritmeticSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the special character \n adds an extra blank line
+            //it is called the next line character
+            Console.WriteLine("Arithmetic Samples\n");
+
+            //declare a variable of datatype double
+            //syntax;  datatype variablename assignment operator literal numeric value;
+            //                 double                =                 0,0;
+            double Total = 0.0;
+            double Cost = 15.23;
+            double Tax = 0.81;
+            
+            //arithmetic assignment operation
+            //syntax; receiving variable assignment operator binary arithmetic expression (Addition)
+            //                Total                =                Cost + Tax;
+            //action: the expression will be executed and the result will be placed
+            //into the receiving variable
+            Total = Cost + Tax;
+
+            Console.WriteLine($"The result of {Cost} + {Tax} is {Total}");
+        }
+    }
+}
---
+++ InputOutputMathSolution/InputOutputMath/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace InputOutputMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*Implement a temperature convertor from
+            *Celsius to fahrenheit;
+            *
+            *Jan21
+            */
+         
+
+         //input: Celsius temperature
+         // string inputTemp
+         // double theCelsiusTemperature
+
+         //output: Fahrenheit temperature
+         // double theFahrenheit temperature
+
+         //expression (calculate): (ct * (9.0/5.0)) + 32
+         //check with ct = 0 expect ft = 32
+         //check with ct = 100 expect ft = 212 
+         //check with ct = -40 expect ft = -40
+
+         //promt for a celsius temperature 
+         // .Write() keeps your cursor on the same line
+         Console.WriteLine("Enter a Celsius temperature: ");
+
+            //How does the program pull in the entry from the user
+            //to obtain the key stokes that the user types (input)
+            // .ReadLine()
+            //important!!!
+            // data comes into the program as a string
+            // Does NOT matter if you enter a number, it is treated
+            // as a string
+
+            //declare the variable: datatype variablename;
+            string inputTemp;
+            //asignment statement:        expression on the right is place
+            //                             into the variable on the left
+            //expression (action):         read the user input
+            inputTemp = Console.ReadLine();
+
+            //you could do both statements on one line
+            //string inputTemp = Console.Readline();
+
+            //currently the celsius value is a string
+            //the value needs to be converted to a number to be
+            //       used in a math calcualtion
+            //convert the data to a different datatype
+            //to do this; you will use technique called parsing
+            // syntax: datatypeTo.Parse(string value)
+            
+            //WARNING: I am assuming the user will enter valid data
+            //             if the user does not enter a number this program
+            //                will abort on the execution of this line
+           
+            double theCelsiustemperature = double.Parse(inputTemp);
+
+            //calcualtion using the conversion expression
+            double theFahrenheitTemperature =
+                              (theCelsiustemperature * (9.0 / 5.0)) + 32;
+
+            //output
+            // .Writeline() which automaticaly foes to the next line.
+            Console.WriteLine($"The Celsius temperature of the {theCelsiustemperature} is ");
+
+        }
+    }
+}
