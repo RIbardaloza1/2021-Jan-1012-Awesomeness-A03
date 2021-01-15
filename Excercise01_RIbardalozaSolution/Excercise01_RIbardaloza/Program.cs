28de53a1485badb842d60ddf72ddb608d95ae768
RIbardaloza1 <77073160+RIbardaloza1@users.noreply.github.com>
2021-01-15 12:13:23 -0700

    forgot to add \n space in between information.

--- Excercise01_RIbardalozaSolution/Excercise01_RIbardaloza/Program.cs
+++ Excercise01_RIbardalozaSolution/Excercise01_RIbardaloza/Program.cs
@@ -5,10 +5,10 @@ namespace Excercise01_RIbardaloza
     class Program
     {
         static void Main(string[] args)
-        {
+        {//forgot to make spaces 
             Console.WriteLine("Hello stranger, my name is Russell Ibardaloza and I am in" +
-                " the Computer Software Development here at NAIT.");
-            Console.WriteLine("Fun fact about my self I am very good at playing League of Legends.");
+                " the Computer Software Development here at NAIT.\n");
+            Console.WriteLine("Fun fact about my self I am very good at playing League of Legends.\n");
             Console.WriteLine("I am expecting a passing grade in this class so that I can get on to my" +
                 " next required courses and pass those as well.");
           
