fb8f9b55437c0e5c10b327ba91278ece1a7744e7
RIbardaloza1 <77073160+RIbardaloza1@users.noreply.github.com>
2021-01-15 11:46:33 -0700

    Write a program that will output describing yourself

---
+++ Excercise01_RIbardalozaSolution/Excercise01_RIbardaloza/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Excercise01_RIbardaloza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello stranger, my name is Russell Ibardaloza and I am in" +
+                " the Computer Software Development here at NAIT.");
+            Console.WriteLine("Fun fact about my self I am very good at playing League of Legends.");
+
+            Console.WriteLine("I am expecting a passing grade in this class so that I can get on my" +
+                "  next required course and pass those as well.");
+
+            
+        }
+    }
+}
